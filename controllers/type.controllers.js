ac24a3d6be9dccaf7a695dc2fca940b2077892ea
ManhPM <phammanhbeo2001@gmail.com>
2023-03-03 23:54:12 +0700

    lastest

--- controllers/type.controllers.js
+++ controllers/type.controllers.js
@@ -1,6 +1,49 @@
 const { Type } = require("../models");
 const { QueryTypes } = require("sequelize");
 
+const createType = async (req, res) => {
+  const { name } = req.body
+  try {
+      await Type.create({ name })
+      res.status(201).json({message: "Tạo mới  thành công!"})
+  } catch (error) {
+      res.status(500).json({message: "Đã có lỗi xảy ra!"})
+  }
+}
+
+const updateType = async (req, res) => {
+  const { id_type } = req.params
+  const { name } = req.body
+  try {
+      const typeUpdate = await Type.findOne({
+        where: {
+          id_type
+        }
+      })
+      typeUpdate.name = name
+      await typeUpdate.save();
+      res.status(201).json({message: "Cập nhật thành công!"})
+  } catch (error) {
+      res.status(500).json({message: "Đã có lỗi xảy ra. Cập nhật thất bại!"})
+  }
+}
+
+const deleteType = async (req, res) => {
+  const { id_type } = req.params
+  try {
+      const typeUpdate = await Type.findOne({
+        where: {
+          id_type
+        }
+      })
+      typeUpdate.status = 0;
+      await typeUpdate.save();
+      res.status(201).json({message: "Xoá thành công!"})
+  } catch (error) {
+      res.status(500).json({message: "Đã có lỗi xảy ra. Xoá thất bại!"})
+  }
+}
+
 const getAllType = async (req, res) => {
   try {
     const typeList = await Type.sequelize.query(
@@ -19,4 +62,7 @@ const getAllType = async (req, res) => {
 
 module.exports = {
     getAllType,
+    createType,
+    updateType,
+    deleteType,
 };
--- migrations/20230111030302-create-type.js
+++ migrations/20230111030302-create-type.js
@@ -12,6 +12,11 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING,
       },
+      status: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        defaultValue: 1,
+      }
     });
   },
   async down(queryInterface, Sequelize) {

6a1c4928196a52a0a421f93dd065da567656f659
ManhPM <phammanhbeo2001@gmail.com>
2023-04-27 18:33:48 +0700

    fix getItem

--- seeders/20230114131428-order.js
+++ seeders/20230114131428-order.js
@@ -16,37 +16,43 @@ module.exports = {
         id_payment: 2,
         id_customer: 2,
         datetime: "2023-01-01 08:30:00",
-        description: "Lấy thêm nước chấm giúp em ạ!"
+        description: "Lấy thêm nước chấm giúp em ạ!",
+        status: 1,
       },
       {
         id_payment: 1,
         id_customer: 1,
         datetime: "2023-01-15 21:30:00",
-        description: "Không cần lấy tương ớt nhé shop!"
+        description: "Không cần lấy tương ớt nhé shop!",
+        status: 1,
       },
       {
         id_payment: 2,
         id_customer: 2,
         datetime: "2023-01-16 09:30:00",
-        description: "Đóng gói kĩ giúp em!"
+        description: "Đóng gói kĩ giúp em!",
+        status: 1,
       },
       {
         id_payment: 3,
         id_customer: 2,
         datetime: "2023-01-17 11:30:00",
-        description: "Không bỏ đồ chua nhé!"
+        description: "Không bỏ đồ chua nhé!",
+        status: 0
       },
       {
         id_payment: 3,
         id_customer: 2,
         datetime: "2023-01-20 06:30:00",
-        description: "Không bỏ đồ chua nhé!"
+        description: "Không bỏ đồ chua nhé!",
+        status: 1,
       },
       {
         id_payment: 3,
         id_customer: 1,
         datetime: "2023-01-23 06:30:00",
-        description: "Không bỏ đồ chua nhé!"
+        description: "Không bỏ đồ chua nhé!",
+        status: 1,
       },
     ]);
   },

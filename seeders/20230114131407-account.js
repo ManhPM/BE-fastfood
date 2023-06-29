e7805a17faa8cf8c370ce3a9476a903b791a2d3d
ManhPM <phammanhbeo2001@gmail.com>
2023-06-29 08:20:51 +0700

    29/06/2023

--- seeders/20230114131407-account.js
+++ seeders/20230114131407-account.js
@@ -16,28 +16,24 @@ module.exports = {
         id_role: 1,
         username: "khach",
         password: "$2a$10$pVN6f.l9WXqsQxifG89kTOewLKmN6BxXjFoqIUra5MIBcc6Z8yhtW",
-        isActive: 0,
         forgot: 0,
       },
       {
         id_role: 2,
         username: "admin123",
         password: "$2a$10$pVN6f.l9WXqsQxifG89kTOewLKmN6BxXjFoqIUra5MIBcc6Z8yhtW",
-        isActive: 1,
         forgot: 0,
       },
       {
         id_role: 2,
         username: "admin",
         password: "$2a$10$pVN6f.l9WXqsQxifG89kTOewLKmN6BxXjFoqIUra5MIBcc6Z8yhtW",
-        isActive: 1,
         forgot: 0,
       },
       {
         id_role: 1,
         username: "khachhang1",
         password: "$2a$10$pVN6f.l9WXqsQxifG89kTOewLKmN6BxXjFoqIUra5MIBcc6Z8yhtW",
-        isActive: 1,
         forgot: 0,
       },
     ]);
